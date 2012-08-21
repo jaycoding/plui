b2bd2e0aa6cd4eb446fd01b13a80e88c0aeede37
jaylearnstocode <jaylearnstocode@gmail.com>
2012-08-21 23:47:31 +0800

    update

---
+++ js/site/order.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	$(".order-form > section").click(function(){
+		var $this = $(this);		
+		var $next = $this.next("section");
+		if ($next && $next.hasClass("none")) {
+			$next.fadeIn(400, function(){$(this).removeClass("none")});
+		}
+		
+	});
+	
+});
+
+
