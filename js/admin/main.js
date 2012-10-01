08233749522e40710c7b4deb56dff248eaa963fc
jay <jaylearnstocode@gmail.com>
2012-10-02 00:02:25 +0800

    update

---
+++ js/admin/main.js
@@ -0,0 +1,44 @@
+$(function(){
+	//Pantone color select
+	$('.pms-list > .pms').bind("click", function(){
+	    if ($(this).parent().hasClass("readonly")) {
+	        return;
+	    }
+	    
+		$(this).siblings(".pms").removeClass("selected");
+		$(this).addClass("selected");
+	});
+	
+	$(".with-tooltop").tooltip();
+	
+	//Datepicker
+	$('.date').datepicker();
+	
+	$('.btn-advanced-filter').bind("click", function(){
+		var $me = $(this),
+			$filter = $me.closest('.order-list-filter');
+		
+		if (!$filter.find('.expandable').hasClass('expanded')) {
+			$me.find('i').text("-");
+			$filter.find('.expandable').addClass("expanded").slideDown(300);
+		} else {
+			$me.find('i').text('+');
+			$filter.find('.expandable').removeClass("expanded").slideUp(300);
+		}
+	});
+	
+	$('.btn-expand-order').bind("click", function(){
+		var $me = $(this),
+			$order = $me.closest('.type-order');
+		
+		if (!$order.next('.type-job').hasClass('expanded')) {
+			$me.text("-");
+			$order.next('.type-job').addClass("expanded").fadeIn(300);
+		} else {
+			$me.text('+');
+			$order.next('.type-job').removeClass("expanded").fadeOut(300);
+		}
+	});
+});
+
+
