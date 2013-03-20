d87ecaa8c5fbfed8e38b6eafcd6b41cb2302a0bd
Norman Li <norman.li@ef.com>
2013-03-21 03:36:20 +0800

    .

--- js/admin/main.js
+++ js/admin/main.js
@@ -606,6 +606,36 @@ $(function(){
 		tag.remove();
 	});
 
+
+
+	$('textarea.rte-simple').tinymce({
+
+		// Location of TinyMCE script
+		script_url : '../../js/utils/jquery.tinymce/3.5.8/tiny_mce.js',
+
+		width : "100%",
+
+		// General options
+		theme : "advanced",
+		plugins : "autoresize,autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+
+		// Theme options
+		theme : "advanced",
+	    theme_advanced_buttons1 : "mylistbox,mysplitbutton,bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,undo,redo,link,unlink",
+	    theme_advanced_buttons2 : "",
+	    theme_advanced_buttons3 : "",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_statusbar_location : "bottom",
+		theme_advanced_resizing : false,
+
+		autoresize_min_height: "100",
+		autoresize_max_height: "200"
+	});
+
+
+
+
 });
 
 
