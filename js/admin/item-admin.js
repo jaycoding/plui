8e408aa2f461550364d6888cab38efd3702bd6d2
Norman Li <norman.li@ef.com>
2013-09-15 20:43:30 +0800

    .

--- js/admin/item-admin.js
+++ js/admin/item-admin.js
@@ -7,9 +7,61 @@ $(function(){
 		});
 	}
 
+	if ($('#btn-add-main-image').length > 0) {
+		$('#btn-add-main-image').fancybox({
+			width: 580,
+			height: 610,
+			href: "popup-add-image.html"
+		});
+	}
+
+	if ($('#btn-add-imprint-image').length > 0) {
+		$('#btn-add-imprint-image').fancybox({
+			width: 580,
+			height: 610,
+			href: "popup-add-image.html"
+		});
+	}
+
+
 	$(".sortable-img-list").sortable({
 	    forcePlaceholderSize: true,
 	    connectWith: ".sortable-connected",
 	    tolerance:"pointer"
 	});
+
+	$(".box .btn-edit").bind("click", function(){
+		var box = $(this).closest(".box");
+		var boxContentView = box.find(".box-content.view");
+		var boxContentEdit = box.find(".box-content.editing");
+
+		boxContentView.addClass("none");
+		boxContentEdit.removeClass("none");
+
+		return false;
+	});
+
+	$(".box .btn-save").bind("click", function(){
+		var box = $(this).closest(".box");
+		var boxContentView = box.find(".box-content.view");
+		var boxContentEdit = box.find(".box-content.editing");
+
+		boxContentView.removeClass("none");
+		boxContentEdit.addClass("none");
+
+		return false;
+	});
+
+	$(".box .btn-cancel").bind("click", function(){
+		var box = $(this).closest(".box");
+		var boxContentView = box.find(".box-content.view");
+		var boxContentEdit = box.find(".box-content.editing");
+
+		boxContentView.removeClass("none");
+		boxContentEdit.addClass("none");
+
+		return false;
+	});
+
+
 });
