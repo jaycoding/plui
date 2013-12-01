02d2ca3a123ae9e495a6fc4d1f6967f56536012e
Nm <jaylearnstocode@gmail.com>
2013-12-02 01:10:24 +0800

    .

--- js/site/site.js
+++ js/site/site.js
@@ -1,4 +1,21 @@
 $(function(){
 	$("#home-slide").carousel();
 
+	$(".header-search input:text").focus(function(){
+		$(this).animate({width: 370}, 200);
+	});
+
+	$(".header-search input:text").blur(function(){
+		$(this).animate({width: 270}, 200);
+	});
+
+	$(".navi .extend-sub-navi").mouseover(function(){
+		$(".navi-sub").show();
+	});
+
+	$(".navi .navi-sub").mouseleave(function(){
+		$(".navi-sub").hide();
+	});
+
+
 });
