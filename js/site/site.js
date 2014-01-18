4b97f1e0bf73350d3262a2d85a74030d4b6ea587
Nm <jaylearnstocode@gmail.com>
2014-01-18 10:42:48 +0800

    .

--- js/site/site.js
+++ js/site/site.js
@@ -21,4 +21,24 @@ $(function(){
 		$('.scrolltofixed').scrollToFixed();
 	}
 
+	$(".btn-faq-question").live("click", function(){
+	    $(this).addClass("none").siblings(".form-faq-question").removeClass("none");
+	});
+
+	$(".btn-faq-answer").live("click", function(){
+	    $(this).closest(".faq-actions").siblings(".form-faq-answer").removeClass("none");
+	});
+
+	$(".btn-faq-add-file").live("click", function(){
+	    $(this).closest(".faq-actions").siblings(".form-faq-add-file").removeClass("none");
+	});
+
+	$(".btn-faq-edit").live("click", function(){
+	    $(this).closest(".faq-actions").siblings(".form-faq-edit").removeClass("none");
+	});
+
+	$(".faq-upload .btn-add-upload").live("click", function(){
+	    $(this).before('<br><a href="#" class="btn-faq-upload">Upload the file</a> ');
+	});
+
 });
